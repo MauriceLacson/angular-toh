c5113555213eb46803f1969c36d2f174847db8af
Maurice Lacson <mlacson29@gmail.com>
2017-11-16 05:43:58 +0800

    added dashboard component, refactored detail view

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,23 +1,17 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css'],
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class HeroesComponent implements OnInit {
-  // selectedHero: Hero;
-  heroes: Hero[];
+export class DashboardComponent implements OnInit {
+  heroes: Hero[] = [];
 
   constructor(private heroService: HeroService) { }
-
-  // onSelect(hero: Hero): void {
-  //   this.selectedHero = hero;
-  // }
 
   ngOnInit() {
     this.getHeroes();
@@ -25,6 +19,6 @@ export class HeroesComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
+      .subscribe(heroes => this.heroes = heroes.slice(1,5));
   }
 }
